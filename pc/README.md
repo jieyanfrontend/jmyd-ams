b6cb4aa676b53b6b17f0fa087337b52ede73c159
lifuzhao100 <lifuzhao1995@qq.com>
2018-04-19 18:13:46 +0800

    '配合my-webpack-config使用'

---
+++ pc/README.md
@@ -0,0 +1,13 @@
+# 安装yarn
+# 安装依赖
+```
+yarn install
+```
+#开始编程
+```
+npm start
+```
+# 编译部署
+```markdown
+npm run build
+```
--- pc/webpack.config.js
+++ pc/webpack.config.js
@@ -1,3 +1,2 @@
-module.exports = {
-    
-};
+let initConfig = require('my-webpack-config/lib/index').default;
+module.exports = initConfig({});
